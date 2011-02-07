24961366125f854470406830da772c4ea8046ea5
adammckerlie@gmail.com <adammckerlie@gmail.com>
2011-02-07 12:26:35 -0500

    Added context to the template

--- maintenance/middleware.py
+++ maintenance/middleware.py
@@ -1,6 +1,7 @@
 import maintenance.settings as maintenance_settings
 from datetime import datetime, timedelta
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from models import MaintenancePeriod
 
 class MaintenanceModeMiddleware(object):
@@ -12,18 +13,9 @@ class MaintenanceModeMiddleware(object):
         maintenance = MaintenancePeriod.objects.filter(start_date__lt=now, end_date__gt=now)
         if maintenance and request.META['REMOTE_ADDR'] not in maintenance_settings.SAFE_IP:
             if maintenance[0].template:
-                return render_to_response(maintenance[0].template)
+                return render_to_response(maintenance[0].template, context_instance=RequestContext(request))
             else:
-                return render_to_response(maintenance_settings.TEMPLATE)
+                return render_to_response(maintenance_settings.TEMPLATE, context_instance=RequestContext(request))
 
-        # Checking to see if we should display a message about upcomming maintenance periods
-
-        maintenance = MaintenancePeriod.objects.all()
-        for maint in maintenance:
-            notification = timedelta(minutes=maint.notificiation_period)
-            notify = now + notification 
-            if maint.start_date < notify and maint.start_date > now:
-                print request
-            
         return None
 
